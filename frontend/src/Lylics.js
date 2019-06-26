eb8a7ffd6d35a4b69761310ee36e101b174b8190
ytorii <metalwarrior_type_jp@hotmail.com>
2019-06-27 02:54:07 +0900

    Fix styles:

--- frontend/src/Lylics.js
+++ frontend/src/Lylics.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react'
+
+import { makeStyles } from '@material-ui/core/styles';
 import Slide from '@material-ui/core/Slide'
 import Typography from '@material-ui/core/Typography'
 
+const useStyles = makeStyles(theme => ({
+  lylic: {
+    margin: theme.spacing(1),
+    color: 'dimgray'
+  }
+}));
+
 const Lylics = ({title, children}) => {
+  const classes = useStyles();
 
   const [lylics, setLylics] = useState([])
 
@@ -14,7 +24,7 @@ const Lylics = ({title, children}) => {
       { lylics.map( lylic => (
         <div>
           <Slide direction="up" in={true} timeout={2000}>
-            <Typography variant="h4" component="h4">
+            <Typography variant="h4" component="h4" className={classes.lylic}>
               {lylic}
             </Typography>
           </Slide>
